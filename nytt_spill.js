e054f6df0db926869da4cdbb7800d7e6b9151a12
Crispowe <143943594+Crispowe@users.noreply.github.com>
2023-09-21 19:00:18 +0200

    Update nytt_spill.js

--- nytt_spill.js
+++ nytt_spill.js
@@ -1,1 +1,20 @@
+document.getElementById("gjetteknapp").addEventListener("click", gjettespill);
 
+	function gjettespill() {
+            const minimum = 1;
+            const maksimum = 100;
+            const valgt_tall = Math.round(Math.random() * (maksimum - minimum)) + minimum;
+
+                let gjettet_tall = document.getElementById("gjettefelt").value;
+ 
+           	    if (gjettet_tall == valgt_tall) {
+                 alert("Du gjettet " + gjettet_tall + " , det var riktig ");
+           	        }
+
+           	    else if (gjettet_tall > valgt_tall) {
+             	    alert("Du gjettet " + gjettet_tall + ", det er for høyt");
+           	        }
+           	    else {
+              	    alert("Du gjettet " + gjettet_tall + ", det er for lavt")
+          	        }
+        	};
